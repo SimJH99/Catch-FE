de837e76777dcb96b0da6e9fa834912149cf507d
ticketpaper <dlwhdvy369@naver.com>
2024-04-09 09:20:40 +0900

    [Feat] 계정 생성, 계정리스트 화면 구현 #13

--- src/components/AppSideBar.vue
+++ src/components/AppSideBar.vue
@@ -12,7 +12,7 @@
                       <path stroke-linecap="round" stroke-linejoin="round" d="M7.5 14.25v2.25m3-4.5v4.5m3-6.75v6.75m3-9v9M6 20.25h12A2.25 2.25 0 0 0 20.25 18V6A2.25 2.25 0 0 0 18 3.75H6A2.25 2.25 0 0 0 3.75 6v12A2.25 2.25 0 0 0 6 20.25Z" />
                     </svg>
                     
-                  <li><a href="/" class="py-2 px-4 block">대시보드</a></li>
+                  <li><a href="/dashBoard" class="py-2 px-4 block">대시보드</a></li>
               </ul>
           </li>
           <li class="py-2 px-4 border-b border-gray-200">
