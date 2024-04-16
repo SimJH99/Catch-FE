2aa28271d161ea0ff1943b41cffcc9a0afd262ab
SimJH99 <s99s4481@gmail.com>
2024-04-16 16:12:23 +0900

    게시글 리스트 페이지 제작 (#23)

--- src/components/AppSideBar.vue
+++ src/components/AppSideBar.vue
@@ -1,8 +1,5 @@
 <template>
   <aside class="w-64 bg-white shadow">
-    <div class="flex items-center">
-      <img src="@/assets/logo3.png" class="p-5">
-    </div>
     <nav>
       <ul class="list-disc font-bold text-base md:text-xl">
           <li class="py-2 px-4 border-b border-t border-gray-200 ">
@@ -29,7 +26,7 @@
                   <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6 text-orange-400">
                       <path stroke-linecap="round" stroke-linejoin="round" d="M7.5 8.25h9m-9 3H12m-9.75 1.51c0 1.6 1.123 2.994 2.707 3.227 1.129.166 2.27.293 3.423.379.35.026.67.21.865.501L12 21l2.755-4.133a1.14 1.14 0 0 1 .865-.501 48.172 48.172 0 0 0 3.423-.379c1.584-.233 2.707-1.626 2.707-3.228V6.741c0-1.602-1.123-2.995-2.707-3.228A48.394 48.394 0 0 0 12 3c-2.392 0-4.744.175-7.043.513C3.373 3.746 2.25 5.14 2.25 6.741v6.018Z" />
                   </svg>
-                  <li><a href="/postList" class="py-2 px-4 block">게시글 리스트</a></li>
+                  <li><a href="/complaintList" class="py-2 px-4 block">게시글 리스트</a></li>
               </ul>
           </li>
           <li class="py-2 px-4 border-b mb-1">
