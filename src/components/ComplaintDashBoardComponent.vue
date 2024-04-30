48c390200017041adafeb0fdcf66bf4d87d7b292
SimJH99 <s99s4481@gmail.com>
2024-04-30 17:53:27 +0900

    [feat] 고객 대시보드 제작

--- src/components/ComplaintDashBoardComponent.vue
+++ src/components/ComplaintDashBoardComponent.vue
@@ -46,36 +46,35 @@
           <canvas id="categoryChart"></canvas>
         </div>
       </div>
-    </div>
 
-    <div class="bg-white m-2 p-2 rounded-md shadow-md border">
-      <div class="flex">
-        <div class="m-2 text-2xl font-bold">일별 문의글 작성 수</div>
-        <input type="month" class="ml-3 outline-none" v-model="defaultMonth" :max="maxMonth">
-        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6 self-center cursor-pointer" @click="findMonth()">
-          <path stroke-linecap="round" stroke-linejoin="round" d="m15.75 15.75-2.489-2.489m0 0a3.375 3.375 0 1 0-4.773-4.773 3.375 3.375 0 0 0 4.774 4.774ZM21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z" />
-        </svg>    
+      <div class="bg-white m-2 p-2 rounded-md shadow-md border">
+        <div class="flex">
+          <div class="m-2 text-2xl font-bold">일별 문의글 작성 수</div>
+          <input type="month" class="ml-3 outline-none" v-model="defaultMonth" :max="maxMonth">
+          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6 self-center cursor-pointer" @click="findMonth()">
+            <path stroke-linecap="round" stroke-linejoin="round" d="m15.75 15.75-2.489-2.489m0 0a3.375 3.375 0 1 0-4.773-4.773 3.375 3.375 0 0 0 4.774 4.774ZM21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z" />
+          </svg>    
+        </div>
+        <div class="m-2 p-4">
+          <canvas id="monthChart"></canvas>
+        </div>
       </div>
-      <div class="m-2 p-4">
-        <canvas id="monthChart"></canvas>
+
+      <div class="bg-white m-2 p-2 rounded-md shadow-md border">
+        <div class="flex">
+          <div class="m-2 text-2xl font-bold">월별 문의글 작성 수</div>
+          <select id="year" v-model="defaultYear" class="ml-3 outline-none">
+            <option v-for="(year, index) in years" :key="index" :value="year">{{year}}</option>
+          </select>
+          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6 self-center cursor-pointer" @click="findYear()">
+            <path stroke-linecap="round" stroke-linejoin="round" d="m15.75 15.75-2.489-2.489m0 0a3.375 3.375 0 1 0-4.773-4.773 3.375 3.375 0 0 0 4.774 4.774ZM21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z" />
+          </svg>    
+        </div>
+        <div class="m-2 p-4">
+          <canvas id="yearChart"></canvas>
+        </div>
       </div>
     </div>
-
-    <div class="bg-white m-2 p-2 rounded-md shadow-md border">
-      <div class="flex">
-        <div class="m-2 text-2xl font-bold">월별 문의글 작성 수</div>
-        <select id="year" v-model="defaultYear" class="ml-3 outline-none">
-          <option v-for="(year, index) in years" :key="index" :value="year">{{year}}</option>
-        </select>
-        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6 self-center cursor-pointer" @click="findYear()">
-          <path stroke-linecap="round" stroke-linejoin="round" d="m15.75 15.75-2.489-2.489m0 0a3.375 3.375 0 1 0-4.773-4.773 3.375 3.375 0 0 0 4.774 4.774ZM21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z" />
-        </svg>    
-      </div>
-      <div class="m-2 p-4">
-        <canvas id="yearChart"></canvas>
-      </div>
-    </div>
-
 
   </div>
 </template>
