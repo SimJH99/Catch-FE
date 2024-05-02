8ae560998556090bdf11fba5f4d0ad90b07dea3d
ticketpaper <dlwhdvy369@naver.com>
2024-05-02 09:55:45 +0900

    [Feat] 로그인 엔터 가능, 관리자 이신가요? 추가

--- src/views/LoginUser.vue
+++ src/views/LoginUser.vue
@@ -1,52 +1,36 @@
 <template>
-  <div
-    class="min-h-screen flex flex-col items-center justify-center bg-gray-100"
-  >
+  <div class="min-h-screen flex flex-col items-center justify-center bg-gray-100">
     <div class="bg-white p-8 rounded-md shadow-md max-w-md w-full">
       <h2 class="text-3xl font-semibold mb-6 text-center text-gray-800">
         회원 로그인
       </h2>
 
       <div class="mb-4">
-        <label
-          for="employeeNumber"
-          class="block text-sm font-medium text-gray-600"
-          >이메일</label
-        >
-        <input
-          v-model="email"
-          type="text"
-          id="employeeNumber"
-          name="employeeNumber"
-          class="mt-1 p-3 w-full border rounded-md focus:outline-none focus:border-indigo-500"
-        />
+        <label for="employeeNumber" class="block text-sm font-medium text-gray-600">이메일</label>
+        <input v-model="email" type="text" id="employeeNumber" name="employeeNumber"
+          class="mt-1 p-3 w-full border rounded-md focus:outline-none focus:border-indigo-500" />
       </div>
 
       <div class="mb-6">
-        <label for="password" class="block text-sm font-medium text-gray-600"
-          >비밀번호</label
-        >
-        <input
-          v-model="password"
-          type="password"
-          id="password"
-          name="password"
-          class="mt-1 p-3 w-full border rounded-md focus:outline-none focus:border-indigo-500"
-        />
+        <label for="password" class="block text-sm font-medium text-gray-600">비밀번호</label>
+        <input v-model="password" type="password" id="password" name="password"
+          class="mt-1 p-3 w-full border rounded-md focus:outline-none focus:border-indigo-500" />
       </div>
 
       <form @submit.prevent="doLogin" class="w-full">
-        <button
-          @click="sendCode"
-          type="submit"
+        <button @click="sendCode" type="submit"
           class="bg-yellow-500 text-white px-4 py-3 rounded-md hover:bg-gray-900 focus:outline-none focus:shadow-outline-gray active:bg-gray-800 w-full"
-          style="background-color: #f5a742"
-        >
+          style="background-color: #f5a742">
           로그인
         </button>
 
-        <div class="mt-4 flex w-full items-center justify-end space-x-4">
-          <p>계정 관련 문의는 관리자에게 문의하세요</p>
+        <div class="mt-4 flex w-full items-center justify-between">
+          <div>
+            <a href="#" class="text-sm text-gray-500 hover:text-gray-700">아이디 찾기</a>
+            <span class="text-gray-500 mx-1">|</span>
+            <a href="#" class="text-sm text-gray-500 hover:text-gray-700">비밀번호 찾기</a>
+          </div>
+          <a href="/admin/login" class="text-sm text-gray-500 hover:text-gray-700">관리자 이신가요?</a>
         </div>
       </form>
     </div>
@@ -56,7 +40,7 @@
 <script>
 import axios from "axios";
 import { initializeApp } from "firebase/app";
-import { getMessaging, getToken} from "firebase/messaging";
+import { getMessaging, getToken } from "firebase/messaging";
 
 
 export default {
@@ -86,7 +70,7 @@ export default {
         localStorage.setItem("refresh_token", result.result.refresh_token);
 
         // test
-            
+
         const firebaseConfig = {
           apiKey: `${process.env.VUE_APP_FIREBASE_API_KEY}`,
           authDomain: `${process.env.VUE_APP_FIREBASE_AUTH_DOMAIN}`,
@@ -96,32 +80,32 @@ export default {
           appId: `${process.env.VUE_APP_FIREBASE_APP_ID}`,
           measurementId: `${process.env.VUE_APP_FIREBASE_MEASUREMENTID}`
         }
-            
-      const firebase = initializeApp(firebaseConfig);
-      const messaging = getMessaging(firebase);
 
-      getToken(messaging,{
-        vapidKey: `${process.env.VUE_APP_FIREBASE_VAP_ID}`
-      })
-      .then((token) => {
-      const access_token = localStorage.getItem('access_token');
-      const headers = access_token ? {Authorization: `Bearer ${access_token}`} : {};
-      axios.post(
-          `${process.env.VUE_APP_API_BASE_URL}/user/pushToken`,
-          {
-            email: this.email,
-            pushToken: token,
-          },
-          {headers}
-        );
-        this.pushToken= token;
-        console.log('해당 브라우저에서의 토큰 : ', this.pushToken);
-      })
-      .catch((err) => {
-        console.log(err);
-      });
+        const firebase = initializeApp(firebaseConfig);
+        const messaging = getMessaging(firebase);
 
-      let queryString = window.location.search;
+        getToken(messaging, {
+          vapidKey: `${process.env.VUE_APP_FIREBASE_VAP_ID}`
+        })
+          .then((token) => {
+            const access_token = localStorage.getItem('access_token');
+            const headers = access_token ? { Authorization: `Bearer ${access_token}` } : {};
+            axios.post(
+              `${process.env.VUE_APP_API_BASE_URL}/user/pushToken`,
+              {
+                email: this.email,
+                pushToken: token,
+              },
+              { headers }
+            );
+            this.pushToken = token;
+            console.log('해당 브라우저에서의 토큰 : ', this.pushToken);
+          })
+          .catch((err) => {
+            console.log(err);
+          });
+
+        let queryString = window.location.search;
         console.log(queryString);
         let urlParams = new URLSearchParams(queryString);
         console.log(urlParams);
@@ -129,10 +113,10 @@ export default {
         if (urlParams.has('url')) {
           url = urlParams.get('url');
         }
-        if(url == undefined){
+        if (url == undefined) {
           alert("로그인 되었습니다.");
-          window.location.href = "/mypage";  
-        }else{
+          window.location.href = "/mypage";
+        } else {
           window.location.href = `/${url}`;
         }
       } catch (error) {
@@ -140,8 +124,17 @@ export default {
         alert("입력하신 정보와 일치하는 정보가 없습니다.");
       }
     },
-    
-  },
-};
-</script>
 
+  },
+  mounted() {
+    document.addEventListener('keydown', (event) => {
+      if (event.key === 'Enter') {
+        event.preventDefault(); // 기본 엔터 키 동작을 막음
+        this.doLogin(); // 로그인 함수 호출
+      }
+    });
+  },
+
+};
+
+</script>
