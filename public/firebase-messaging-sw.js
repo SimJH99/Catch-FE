aa4d92011cc86170b04018dea19cbec67a1714b5
ticketpaper <122894395+ticketpaper@users.noreply.github.com>
2024-05-03 19:51:03 +0900

    Update firebase-messaging-sw.js

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -1,34 +1,23 @@
-// Give the service worker access to Firebase Messaging.
-// Note that you can only use Firebase Messaging here. Other Firebase libraries
-// are not available in the service worker.
 importScripts('https://www.gstatic.com/firebasejs/8.10.1/firebase-app.js');
 importScripts('https://www.gstatic.com/firebasejs/8.10.1/firebase-messaging.js');
 
-// Initialize the Firebase app in the service worker by passing in
-// your app's Firebase config object.
-// https://firebase.google.com/docs/web/setup#config-object
 firebase.initializeApp({
-  apiKey: `${process.env.VUE_APP_FIREBASE_API_KEY}`,
-  authDomain: `${process.env.VUE_APP_FIREBASE_AUTH_DOMAIN}`,
-  projectId: `${process.env.VUE_APP_FIREBASE_PROJECT_ID}`,
-  storageBucket: `${process.env.VUE_APP_FIREBASE_STORAGE_BUCKET}`,
-  messagingSenderId: `${process.env.VUE_APP_FIREBASE_MESSAGING_SENDER_ID}`,
-  appId: `${process.env.VUE_APP_FIREBASE_APP_ID}`,
-  measurementId: `${process.env.VUE_APP_FIREBASE_MEASUREMENTID}`
+  apiKey: "AIzaSyCCvpmI_b0r4wz5jaxiB4d9JEmc2MCaIYQ",
+  authDomain: "catch-push.firebaseapp.com",
+  projectId: "catch-push",
+  storageBucket: "catch-push.appspot.com",
+  messagingSenderId: "786691079751",
+  appId: "1:786691079751:web:f827e022f550c46bb93b62",
+  measurementId: "G-5X30Z4MRCE"
 });
-// 2단계 config와 같게 진행
-// Retrieve an instance of Firebase Messaging so that it can handle background
-// messages.
 
 const messaging = firebase.messaging();
 
-// 브라우저가 꺼진후에도 진행되어질수 있도록하는 method
 messaging.onBackgroundMessage((payload) => {   
     console.log(
       '[firebase-messaging-sw.js] Received background message ',
       payload
     );
-    // Customize notification here  
     const notificationTitle = payload.notification.title;
     const notificationOptions = {
       body: payload.notification.body,
