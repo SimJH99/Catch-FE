a7e4b848f48a554afcf841ec790daae7c4dcc7a8
ticketpaper <dlwhdvy369@naver.com>
2024-05-06 16:48:45 +0900

    [Fix] firebase 관련 수정

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -1,28 +1,28 @@
-importScripts('https://www.gstatic.com/firebasejs/8.10.1/firebase-app.js');
-importScripts('https://www.gstatic.com/firebasejs/8.10.1/firebase-messaging.js');
+// importScripts('https://www.gstatic.com/firebasejs/8.10.1/firebase-app.js');
+// importScripts('https://www.gstatic.com/firebasejs/8.10.1/firebase-messaging.js');
 
-// firebase.initializeApp({
-//   apiKey: "AIzaSyCCvpmI_b0r4wz5jaxiB4d9JEmc2MCaIYQ",
-//   authDomain: "catch-push.firebaseapp.com",
-//   projectId: "catch-push",
-//   storageBucket: "catch-push.appspot.com",
-//   messagingSenderId: "786691079751",
-//   appId: "1:786691079751:web:f827e022f550c46bb93b62",
-//   measurementId: "G-5X30Z4MRCE"
-// });
+// // firebase.initializeApp({
+// //   apiKey: "AIzaSyCCvpmI_b0r4wz5jaxiB4d9JEmc2MCaIYQ",
+// //   authDomain: "catch-push.firebaseapp.com",
+// //   projectId: "catch-push",
+// //   storageBucket: "catch-push.appspot.com",
+// //   messagingSenderId: "786691079751",
+// //   appId: "1:786691079751:web:f827e022f550c46bb93b62",
+// //   measurementId: "G-5X30Z4MRCE"
+// // });
 
-const messaging = firebase.messaging();
+// const messaging = firebase.messaging();
 
-messaging.onBackgroundMessage((payload) => {   
-    console.log(
-      '[firebase-messaging-sw.js] Received background message ',
-      payload
-    );
-    const notificationTitle = payload.notification.title;
-    const notificationOptions = {
-      body: payload.notification.body,
-      icon: '/favicon.ico'
-    };
+// messaging.onBackgroundMessage((payload) => {   
+//     console.log(
+//       '[firebase-messaging-sw.js] Received background message ',
+//       payload
+//     );
+//     const notificationTitle = payload.notification.title;
+//     const notificationOptions = {
+//       body: payload.notification.body,
+//       icon: '/favicon.ico'
+//     };
   
-    self.registration.showNotification(notificationTitle, notificationOptions);
-  });
+//     self.registration.showNotification(notificationTitle, notificationOptions);
+//   });
