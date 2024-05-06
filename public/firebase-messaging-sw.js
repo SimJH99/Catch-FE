e8c1c2c80a6761939178af5846e73b42a48edccc
ticketpaper <122894395+ticketpaper@users.noreply.github.com>
2024-05-06 16:14:15 +0900

    initializeApp 제외

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -1,15 +1,15 @@
 importScripts('https://www.gstatic.com/firebasejs/8.10.1/firebase-app.js');
 importScripts('https://www.gstatic.com/firebasejs/8.10.1/firebase-messaging.js');
 
-firebase.initializeApp({
-  apiKey: "AIzaSyCCvpmI_b0r4wz5jaxiB4d9JEmc2MCaIYQ",
-  authDomain: "catch-push.firebaseapp.com",
-  projectId: "catch-push",
-  storageBucket: "catch-push.appspot.com",
-  messagingSenderId: "786691079751",
-  appId: "1:786691079751:web:f827e022f550c46bb93b62",
-  measurementId: "G-5X30Z4MRCE"
-});
+// firebase.initializeApp({
+//   apiKey: "AIzaSyCCvpmI_b0r4wz5jaxiB4d9JEmc2MCaIYQ",
+//   authDomain: "catch-push.firebaseapp.com",
+//   projectId: "catch-push",
+//   storageBucket: "catch-push.appspot.com",
+//   messagingSenderId: "786691079751",
+//   appId: "1:786691079751:web:f827e022f550c46bb93b62",
+//   measurementId: "G-5X30Z4MRCE"
+// });
 
 const messaging = firebase.messaging();
 
